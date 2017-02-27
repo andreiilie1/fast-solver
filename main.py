99ca1297c4989f30a2433f49b2e1824e774947a9
andreiilie1 <andrei0758@gmail.com>
2017-02-27 17:51:02 +0000

    Compute skeleton matrix - not symmetric because border

--- main.py
+++ main.py
@@ -5,34 +5,48 @@ from scipy import *
 # Current version assumes the value on the border is 1
 # Curreny version assumes the nabla operator of f is 0 everywhere
 
+# Check if a(i,j) is on border
 def isOnBorder(i, j):
 	if(i == 0 or j == 0 or i == N - 1 or j == N - 1):
 		return True
 	else:
 		return False
 
+# Get the coordinates of the variable around which the row-th row is created
 def getCoordinates(row):
 	return row / N, row % N
 
-def computeRow(Matrix, row):
+# Get the row of a(i, j)'s equation
+def getRow(i, j):
+	return(i * N + j)
+
+# Compute the elements of row-th row in (rowList, colList, dataList)
+def computeRow(row):
 	(x, y) = getCoordinates(row)
 	if(isOnBorder(x, y)):
-		row.append(row)
-		col.append(row)
-		data.append(1)
+		rowList.append(row)
+		colList.append(row)
+		dataList.append(1)
 	else:
-		row.append(row)
-		col.append(row)
-		data.append(4)
-		
+		rowList.append(row)
+		colList.append(row)
+		dataList.append(4)
+		for (dX, dY) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+			rowList.append(row)
+			colList.append(getRow(x + dX, y + dY))
+			dataList.append(-1)
 
 
 N = input("Enter inverse of sample rate\n")
 
-row = []
-col = []
-data = []
-M = csr_matrix((np.array(data), (np.array(row), np.array(col))), shape = (3, 3))
+rowList = []
+colList = []
+dataList = []
 
-for currentRow in range(N):
+# Process the entries of each row to create matrix M
+for currentRow in range(N * N):
 	computeRow(currentRow)
+
+# Instantiate sparse matrix M according to data kept in (rowList, colList, dataList)
+M = csr_matrix((np.array(dataList), (np.array(rowList), np.array(colList))), shape = (N * N, N * N))
+print(M.toarray())
